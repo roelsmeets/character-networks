c581da9d1829604eecd76d5ac59778e24a4d9746
roelsmeets <r.smeets@let.ru.nl>
2018-11-07 13:15:21 -0500

    Started with the code for modelling winners/losers in enemy-pairs

--- conflict.py
+++ conflict.py
@@ -1,10 +1,16 @@
 # #!/usr/bin/env python
 # # -*- coding: utf-8 -*-
 
+# 1. IMPORTS
+
 from characternetworks import Character, Character_Centrality
 from variables import *
 
 import csv
+
+
+
+# 2. INPUT
 
 allcharacters = {}
 
@@ -41,10 +47,14 @@ with open(csvfiles['rankings'], 'rt') as csvfile1, \
             eigenvector = line[14]
             katz = line[15]
 
+            winlose = 0 
+
             if not book_id in allcharacters:
                 allcharacters[book_id] = {}
-            allcharacters[book_id][character_id] = Character_Centrality(book_id, character_id, name, gender, descent_country, descent_city, living_country, living_city, age, education, profession, degree, betweenness, closeness, eigenvector, katz)
+            allcharacters[book_id][character_id] = Character_Centrality(book_id, character_id, name, gender, descent_country, descent_city, living_country, living_city, age, education, profession, degree, betweenness, closeness, eigenvector, katz, winlose)
 
+
+# 3. COMPUTE SOCIAL BALANCE
     
     balance = 0
     imbalance = 0
@@ -121,10 +131,12 @@ with open(csvfiles['rankings'], 'rt') as csvfile1, \
             try:
                 for enemy in enemies[book_id][source]:
                     if enemy in enemies[book_id][target]:
+                        imbalance += 1
                         print ('1: falsified',book_id,source,target,enemy,relation_types[book_id][(source,target)],relation_types[book_id][(source,enemy)],relation_types[book_id][(enemy,target)])
                 if source in friends[book_id] and target in friends[book_id]:
                     for friend in friends[book_id][source]:
                         if friend in friends[book_id][target]:
+                            imbalance += 1
                             print ('2: falsified',book_id,source,target,friend,relation_types[book_id][(source,target)],relation_types[book_id][(source,friend)],relation_types[book_id][(friend,target)])
             except:
                 print('ERROR:',book_id,source,target)
@@ -135,12 +147,68 @@ with open(csvfiles['rankings'], 'rt') as csvfile1, \
                 if source in enemies[book_id] and target in enemies[book_id]:
                     for enemy in enemies[book_id][source]:
                         if enemy in enemies[book_id][target]:
+                            balance += 1
                             print ('3: verified', book_id,source,target,enemy,relation_types[book_id][(source,target)],relation_types[book_id][(source,enemy)],relation_types[book_id][(enemy,target)])
                     for friend in friends[book_id][source]:
                         if friend in friends[book_id][target]:
+                            balance += 1
                             print ('4: verified',book_id,source,target,friend, relation_types[book_id][(source,target)],relation_types[book_id][(source,friend)],relation_types[book_id][(friend,target)])
             except:
                 print('ERROR:',book_id,source,target)
+
+    print ('Balanced triads =', balance)
+    print ('Imbalanced triads =', imbalance)
+
+
+# 4. COMPUTE ENEMY HIERARCHIES
+
+
+
+    # Strategy 1 
+    for book_id in allcharacters:
+        for character_id in allcharacters[book_id]:
+            for (source, target) in enemypairs[book_id]:
+                if allcharacters[source] and allcharacters[target] in enemypairs[book_id]:
+                    # print ('source:', allcharacters[source])
+                    # print ('target:', allcharacters[target])
+                    if allcharacters[book_id][source].degree > allcharacters[book_id][target].degree:
+                        allcharacters[book_id][source].winlose += 1
+                    print ('book_id source =', allcharacters[book_id][source].book_id, 'character_id source =', allcharacters[book_id][source].character_id, 'degree source =', allcharacters[book_id][source].degree,  'source winlose score degree=', allcharacters[book_id][source].winlose)
+                    if allcharacters[book_id][target].degree > allcharacters[book_id][source].degree:
+                        allcharacters[book_id][target].winlose += 1
+                        print ('book_id target =', allcharacters[book_id][target].book_id, 'character_id target =', allcharacters[book_id][target].character_id, 'degree target =', allcharacters[book_id][target].degree,  'target winlose score degree=', allcharacters[book_id][target].winlose)
+
+
+
+    
+
+    
+    # Strategy 2
+    # for book_id in enemypairs:
+    #     for (source, target) in enemypairs[book_id]:
+    #         for character_id in allcharacters[book_id][source]:
+    #             if character_id in allcharacters[book_id][target]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
