3d8f486ef5b903643f000bdfc796b1f3d7798ea6
roelsmeets <r.smeets@let.ru.nl>
2020-01-10 15:53:03 +0100

    Code to compute the means of the assortativity scores of all permutations

---
+++ compute_means_permutations.py
@@ -0,0 +1,26 @@
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+
+import csv
+import pandas as pd
+import glob
+
+path = r'/Users/roelsmeets/Desktop/Libris_networks/character-networks/character-networks/permutations' 
+all_files = glob.glob(path + "/*.txt")
+
+print (all_files)
+
+file_list = []
+
+for filename in all_files:
+    df = pd.read_csv(filename, header=None)
+    file_list.append(df)
+
+frame = pd.concat(file_list)
+frame.columns = ['gender_assortativity', 'age_assortativity', 'education_assortativity', 'descent_assortativity']
+
+print (frame.mean())
+
+
+
+
